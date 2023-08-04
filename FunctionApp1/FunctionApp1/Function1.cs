2c093afa36c251e2aef6a371fbe5f961e826ffec
nguyenan97 <38469313+nguyenan97@users.noreply.github.com>
2023-08-04 16:14:03 +0700

    Update Function1.cs

--- FunctionApp1/FunctionApp1/Function1.cs
+++ FunctionApp1/FunctionApp1/Function1.cs
@@ -2,7 +2,7 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-//123123333333333333
+//123123333333333333 123123
 namespace FunctionApp1
 {
     public class Function1
